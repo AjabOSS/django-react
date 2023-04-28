02bb3d7f5d7ce009743e6957e91079aea1ceed0e
arman <79183672+TikTaak@users.noreply.github.com>
2023-04-28 21:25:15 +0430

    add def base view #

--- books/views.py
+++ books/views.py
@@ -11,3 +11,18 @@ class BookDetail(RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookDetailSerializer
     
+# class BookList(APIView):
+#     """
+#     List all snippets, or create a new snippet.
+#     """
+#     def get(self, request, format=None):
+#         snippets = Snippet.objects.all()
+#         serializer = SnippetSerializer(snippets, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request, format=None):
+#         serializer = SnippetSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
