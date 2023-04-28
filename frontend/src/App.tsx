66392d4c1b98d67b67139a7af099687c2f579361
Ali Shahmansouri <sctshahali@gmail.com>
2023-04-28 18:28:30 +0430

    Add a simple get request

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    axios.get("http://localhost:8000/api").then((res) => console.log(res));
+  }, []);
 
   return (
     <>
@@ -29,7 +34,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
